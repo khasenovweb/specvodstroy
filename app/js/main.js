5d9dcd2d16950b8bee9ebf95752fdf64a40fb86c
khasenovweb <hasenovkirill93@gmail.com>
2023-01-14 19:10:27 +0500

    pc

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,8 @@
 @@include('libs/parallax.min.js')
 @@include('libs/validate.js')
 @@include('libs/scroll.js')
+@@include('libs/datepicker.min.js')
+@@include('libs/datepicker.ru-RU.js')
 // ==============================//
 // ======= END Libs =============//
 // ==============================//
@@ -57,5 +59,25 @@ $(document).ready(function(){
             offset: 1000,
         });
     }
+
+
+    $.fn.datepicker.languages['ru-RU'] = {
+        format: 'dd.mm.YYYY',
+        days: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+        daysShort: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+        daysMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+        months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        monthsShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+        weekStart: 1,
+        startView: 0,
+        yearFirst: false,
+        yearSuffix: ''
+    };
+    
+    
+    $('[data-datepicker]').datepicker({
+        language: 'ru-RU',
+        format: 'dd.mm.yyyy'
+    });
     
 });
